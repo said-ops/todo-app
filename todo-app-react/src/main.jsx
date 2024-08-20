cd2d68562067675463047f92e0b7be93540f571c
said-ops <idrissisaid29@gmail.com>
2024-08-20 22:08:50 +0100

    base layout

--- todo-app-react/src/main.jsx
+++ todo-app-react/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App.jsx'
 import './styles/reset.css'
-// import './styles/index.css'
+import './styles/main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
